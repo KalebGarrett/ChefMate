7ea7b570dec36315b414b33bc17ade84f7d761b5
Kaleb Garrett <kalebagarrett@gmail.com>
2023-03-14 22:34:27 -0400

    Scaffolded recipe controller

---
+++ FishWatch.API/Controllers/RecipeController.cs
@@ -0,0 +1,42 @@
+﻿using ChefMate.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FishWatch.API.Controllers;
+
+[ApiController]
+[Route("recipes")]
+public class RecipeController : ControllerBase
+{
+    [HttpGet("")]
+    public async Task<IActionResult> Get()
+    {
+        var recipes = new List<Recipe>();
+        recipes.Add(new Recipe());
+        recipes.Add(new Recipe());
+        return Ok(recipes);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> Get(string id)
+    {
+        return Ok(new Recipe());
+    }
+
+    [HttpPost("")]
+    public async Task<IActionResult> Create(Recipe recipe)
+    {
+        return Created("", recipe);
+    }
+    
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Update(string id, Recipe recipe)
+    {
+        return Ok(recipe);
+    }
+    
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(string id)
+    {
+        return NoContent();
+    }
+}
