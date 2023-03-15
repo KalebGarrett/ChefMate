f99557bf97a2e8081bbaeca0b6477ea2bc9a5c18
Kaleb Garrett <kalebagarrett@gmail.com>
2023-03-14 22:08:02 -0400

    Created new solution and class library project

---
+++ ChefMate.Models/Recipe.cs
@@ -0,0 +1,22 @@
+﻿namespace ChefMate.Models;
+
+public class Recipe
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public List<Ingredient> Ingredients { get; set; } = new List<Ingredient>();
+    public List<RecipeStep> Steps { get; set; } = new List<RecipeStep>();
+}
+
+public class Ingredient
+{
+    public string Name { get; set; }
+    public double Amount { get; set; }
+    public string Unit { get; set; }
+}
+
+public class RecipeStep
+{
+    public string Description { get; set; }
+}
