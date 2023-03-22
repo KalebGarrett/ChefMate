4e609e70d4afa501b904e603ddd05db27d310ab9
Kaleb Garrett <kalebagarrett@gmail.com>
2023-03-21 21:34:28 -0400

    Created recipe page and launched API

---
+++ ChefMate.App/Services/RecipeService.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json;
+using ChefMate.Models;
+
+namespace ChefMate.App.Services;
+
+public class RecipeService
+{
+    private readonly HttpClient _httpClient;
+    
+    public RecipeService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+    
+    public async Task<List<Recipe>> GetAll()
+    {
+        var result = await _httpClient.GetAsync("recipes");
+        if (result.IsSuccessStatusCode)
+        {
+            var json = await result.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<List<Recipe>>(json, new JsonSerializerOptions()
+            {
+               PropertyNameCaseInsensitive = true
+            });
+        }
+
+        return new List<Recipe>();
+    }
+}
